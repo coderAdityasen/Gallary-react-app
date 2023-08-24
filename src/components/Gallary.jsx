ad987dd6f1d03420d748cebc06fbff41b3bdbe85
Aditya sen <adityasen561@gamil.com>
2023-08-24 17:28:13 +0530

    loading animation comes with image gallary

--- src/components/Gallary.jsx
+++ src/components/Gallary.jsx
@@ -1,67 +1,73 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"; // fetch api from server 
-import "./ImageGallery.scss"; // internal file 
-import { Link } from "react-router-dom"; // npm install react-router-dom , dom inside react
+import axios from "axios";
+import "./ImageGallery.scss";
+import { Link } from "react-router-dom";
 
 const ImageGallery = () => {
   const [images, setImages] = useState([]);
   const [offset, setOffset] = useState(0);
-  const [Loading , setLoading] = useState(false)
+  const [Loading, setLoading] = useState(false);
   const url = `https://api.slingacademy.com/v1/sample-data/photos?offset=${offset}&limit=20`;
   useEffect(() => {
     const fetchImage = async () => {
-      setLoading(true)
-        try {
-            const getimage = await axios.get(url);
-      setImages(getimage.data.photos);
-      setLoading(false)
-        } catch (error) {
-            console.log("you have got an error while fetching ", error);
-        }
-     
+      setLoading(true);
+      try {
+        const getimage = await axios.get(url);
+        setImages(getimage.data.photos);
+        setLoading(false);
+      } catch (error) {
+        console.log("you have got an error while fetching ", error);
+      }
     };
     fetchImage();
   }, [offset]);
 
   const handleLoadMore = () => {
-    setOffset(prevOffset => prevOffset + 20); 
+    setOffset((prevOffset) => prevOffset + 20);
   };
 
   return (
     <>
-    
-    <h1>Gallery View</h1>
-    {Loading ?  <div class="loader">
-	<div class="loader-inner">
-		<div class="loader-line-wrap">
-			<div class="loader-line"></div>
-		</div>
-		<div class="loader-line-wrap">
-			<div class="loader-line"></div>
-		</div>
-		<div class="loader-line-wrap">
-			<div class="loader-line"></div>
-		</div>
-		<div class="loader-line-wrap">
-			<div class="loader-line"></div>
-		</div>
-		<div class="loader-line-wrap">
-			<div class="loader-line"></div>
-		</div>
-	</div>
-</div> : <div className="image-gallery">
-      {images.map((image, index) =>(
-        <Link key={index} to={`/gallary/${image.id}`}>
-          <div className="image-container">
-            <img src={image.url} alt={`Image ${index}`} />
+      <h1>Gallery View</h1>
+      {Loading ? (
+        <div class="loader">
+          <div class="loader-inner">
+            <div class="loader-line-wrap">
+              <div class="loader-line"></div>
+            </div>
+            <div class="loader-line-wrap">
+              <div class="loader-line"></div>
+            </div>
+            <div class="loader-line-wrap">
+              <div class="loader-line"></div>
+            </div>
+            <div class="loader-line-wrap">
+              <div class="loader-line"></div>
+            </div>
+            <div class="loader-line-wrap">
+              <div class="loader-line"></div>
+            </div>
           </div>
-          
-        </Link>
-      ))}
-    </div> }
-    
-    <button className="next-btn" onClick={handleLoadMore}>Load More</button>
-  </>
+        </div>
+      ) : (<>
+        <div className="image-gallery">
+          {images.map((image, index) => (
+            <Link key={index} to={`/gallary/${image.id}`}>
+              <div className="image-container">
+                <img src={image.url} alt={`Image ${index}`} />
+              </div>
+            </Link>
+          ))}
+        </div>
+        <div className="button-container">
+        <button className="next-btn" onClick={handleLoadMore}>
+          Load More
+        </button>
+      </div>
+      </>
+      )}
+      
+    </>
   );
 };
 
